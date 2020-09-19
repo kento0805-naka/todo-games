adccb247b1a226b58b70c529a3ce7c6e922f2647
kento0805-naka <kentz.eva.a.r.xxx@ezweb.ne.jp>
2020-09-19 17:40:27 +0900

    他のユーザー画面へのアクセス制限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!, except: [:index]
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     def after_sign_in_path_for(resource)
@@ -9,6 +10,13 @@ class ApplicationController < ActionController::Base
 
     def sign_in_required
         redirect_to new_user_session_url unless user_signed_in?
+    end
+
+    def correct_user
+        if current_user.id != params[:id].to_i
+            flash.alert = "アクセス許可されていません"
+            redirect_to root_path
+        end
     end
 
     protected
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :sign_in_required, only: [:show]
-  
+  before_action :correct_user
+    
   def show
     @user = User.find_by(id: params[:id])
   end
